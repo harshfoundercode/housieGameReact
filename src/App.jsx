eb82d76ce2686cf6f55cfae18965052114ef7414
harshfoundercode <Harsh@foundercodes.com>
2026-04-22 16:26:53 +0530

    all done on 22-4-26

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,48 @@
-import {Routes, Route, Navigate, HashRouter } from "react-router-dom";
+// import {Routes, Route, Navigate, HashRouter } from "react-router-dom";
+// import Home from "./page/Home";
+// import { ROUTES } from "./routes/routes";
+// import GamePage from "./page/BeforeGameLive/gamePage";
+// import AfterGameLive from "./page/AfterGameLive/afterGameLive";
+// import HomeScreenWebsite from "./MainWebsite/homescreen_website";
+// import RulesAndTerms from "./MainWebsite/FooterComponents/rules_regulation_terms";
+// import CancellationRefundPolicy from "./MainWebsite/FooterComponents/cancellation_refund_policy";
+// import PrivacyPolicy from "./MainWebsite/FooterComponents/privacy_policy";
+// import FAQs from "./MainWebsite/FooterComponents/faqs";
+// import Login from "./MainWebsite/AuthComponents/login_screen";
+// import Register from "./MainWebsite/AuthComponents/register_screen";
+
+// function App() {
+//   return (
+//     <HashRouter >
+//       <Routes>
+//         {/* Main Website Landing Page - Opens First */}
+//         <Route path="/" element={<HomeScreenWebsite />} />
+//         <Route path={ROUTES.HomeScreenWebsite} element={<HomeScreenWebsite />} />
+//         <Route path={ROUTES.RULES} element={<RulesAndTerms />} />
+//         <Route path={ROUTES.CANCELATIONREFUNDPOLICY} element={<CancellationRefundPolicy />} />
+//         <Route path={ROUTES.PRIVACYPOLICY} element={<PrivacyPolicy />} />
+//         <Route path={ROUTES.FAQS} element={<FAQs />} />
+//         <Route path={ROUTES.LOGIN} element={<Login />} />
+//         <Route path={ROUTES.REGISTER} element={<Register />} />
+
+
+//         {/* Game Routes - Accessible via navigation from website */}
+//         <Route path="/app" element={<Navigate to={ROUTES.HOME} />} />
+//         <Route path={ROUTES.HOME} element={<Home />} />
+//         <Route path={ROUTES.GAME} element={<GamePage />} />
+//         <Route path={ROUTES.AFTERGAME} element={<AfterGameLive />} />
+
+//         {/* Catch all - Redirect to landing page */}
+//         <Route path="*" element={<Navigate to="/" />} />
+        
+//       </Routes>
+//     </HashRouter >
+//   );
+// }
+
+// export default App;
+
+import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./page/Home";
 import { ROUTES } from "./routes/routes";
 import GamePage from "./page/BeforeGameLive/gamePage";
@@ -13,9 +57,9 @@ import Register from "./MainWebsite/AuthComponents/register_screen";
 
 function App() {
   return (
-    <HashRouter >
+    <HashRouter>
       <Routes>
-        {/* Main Website Landing Page - Opens First */}
+        {/* Public Website Routes */}
         <Route path="/" element={<HomeScreenWebsite />} />
         <Route path={ROUTES.HomeScreenWebsite} element={<HomeScreenWebsite />} />
         <Route path={ROUTES.RULES} element={<RulesAndTerms />} />
@@ -25,17 +69,16 @@ function App() {
         <Route path={ROUTES.LOGIN} element={<Login />} />
         <Route path={ROUTES.REGISTER} element={<Register />} />
 
-
-        {/* Game Routes - Accessible via navigation from website */}
+        {/* Game Routes */}
         <Route path="/app" element={<Navigate to={ROUTES.HOME} />} />
         <Route path={ROUTES.HOME} element={<Home />} />
         <Route path={ROUTES.GAME} element={<GamePage />} />
         <Route path={ROUTES.AFTERGAME} element={<AfterGameLive />} />
 
-        {/* Catch all - Redirect to landing page */}
+        {/* 404 Redirect */}
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
-    </HashRouter >
+    </HashRouter>
   );
 }
 
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,18 +1,31 @@
 
+// export const ROUTES = {
+//   HOME: "/",
+//   GAME: "/gamePage",
+//   AFTERGAME:"/afterGameLive",
+//   RULES: "/rules_regulation_terms",
+//   CANCELATIONREFUNDPOLICY:"/cancellation_refund_policy",
+//   PRIVACYPOLICY:"/privacy_policy",
+//   FAQS:"/faqs",
+//   LOGIN:"/login_screen",
+//   REGISTER:"/register_screen",
+
+//   ///- MAIN Website routes
+
+//   HomeScreenWebsite:"/homescreen_website"
+// };
+
+
+// src/routes/routes.js
 export const ROUTES = {
-  HOME: "/",
-  GAME: "/gamePage",
-  AFTERGAME:"/afterGameLive",
-  RULES: "/rules_regulation_terms",
-  CANCELATIONREFUNDPOLICY:"/cancellation_refund_policy",
-  PRIVACYPOLICY:"/privacy_policy",
-  FAQS:"/faqs",
-  LOGIN:"/login_screen",
-  REGISTER:"/register_screen",
-
-  ///- MAIN Website routes
-
-  HomeScreenWebsite:"/homescreen_website"
+    HomeScreenWebsite: "/",
+    RULES: "/MainWebsite/FooterComponents/rules_regulation_terms",
+    CANCELATIONREFUNDPOLICY: "/MainWebsite/FooterComponents/cancellation_refund_policy",
+    PRIVACYPOLICY: "/MainWebsite/FooterComponents/privacy_policy",
+    FAQS: "/MainWebsite/FooterComponents/faqs",
+    LOGIN: "/MainWebsite/AuthComponents/login_screen",
+    REGISTER: "/MainWebsite/AuthComponents/register_screen",
+    HOME: "/page/Home",
+    GAME: "/page/BeforeGameLive/gamePage",
+    AFTERGAME: "/page/AfterGameLive/afterGameLive",
 };
-
-
